3e571bcc000e21c44931af3b65bd72da002144d8
Natalie Baer <ncbaer@gmail.com>
2014-07-19 18:30:49 -0400

    remove commented code from test

--- toboggan/spec/controllers/users_controller_spec.rb
+++ toboggan/spec/controllers/users_controller_spec.rb
@@ -29,23 +29,6 @@ RSpec.describe UsersController, :type => :controller do
 		  end
 		end
 
-		# 		describe 'User Sign Up' do
-		#   # assumes that there is a FactoryGirl Factory :user
-		#   # with email and password fields.
-		#   let(:user) {create(:user)}
-		 
-		#   it 'allows users to sign in with an email address and password' do
-		#     visit '/users/sign_in'
-		 
-		#     fill_in 'Email', with: user.email
-		#     fill_in 'Password', with: user.password
-		 
-		#     click_button 'Sign in'
-		 
-		#     page.should have_content 'Signed in successfully'
-		#   end
-		# end
-
 		  describe 'User Sign Out' do
 		  it 'allows user to sign out'
 			logout(:user)
