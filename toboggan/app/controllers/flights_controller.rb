be85256513889b87c069d01ec89dcb6fbee708b9
adriansoghoian <adriansoghoian@gmail.com>
2014-07-18 22:41:42 -0400

    Basic form on home page working; to be replaced with Natalie's fancy's JQuery map eventually.

--- toboggan/app/controllers/flights_controller.rb
+++ toboggan/app/controllers/flights_controller.rb
@@ -7,12 +7,18 @@ class FlightsController < ApplicationController
   end
 
   def create
-  	@flight = Flight.create(params[:flight])
+  	@flight = Flight.create(flight_params)
   	redirect_to flight_path(@flight)
   end
 
   def show
 
+  end
+
+  private
+
+  def flight_params
+    params.require(:flight).permit(:beginning_airport, :ending_airport, :search_end, :price)
   end
 
 end
