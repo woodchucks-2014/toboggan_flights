7cdefd2515f79ae899cc02524e8e555a5189e8e0
adriansoghoian <adriansoghoian@gmail.com>
2014-07-19 17:08:55 -0400

    Rearranged the root page map and form.

--- toboggan/app/assets/javascripts/flights.js
+++ toboggan/app/assets/javascripts/flights.js
@@ -2,27 +2,15 @@
   $(document).ready(function() {
     $('#vmap').vectorMap({
         map: 'world_en',
-        backgroundColor: '#6699FF',
-        color: '#ffffff',
+        backgroundColor: '#FFFFFF',
+        color: '#2E4272',
         hoverOpacity: 0.8,
-        selectedColor: '#666666',
+        selectedColor: '#73C988',
         enableZoom: true,
         showTooltip: true,
         values: sample_data,
-        scaleColors: ['#ffb634', '#ff2407'],
+        scaleColors: ['#3A6BDE', '#202B44'],
         normalizeFunction: 'polynomial'
     });
-
-    $( "#slider-range" ).slider({
-      range: true,
-      min: 0,
-      max: 2000,
-      values: [ 75, 300 ],
-      slide: function( event, ui ) {
-        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
-      }
-    });
-    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
   });
 })(jQuery);
