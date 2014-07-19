8a323b97c27d1d8d1c67bcc3094e55fb8a8a65b2
QuigProQuo <johnpeterquigley@gmail.com>
2014-07-18 23:50:12 -0400

    Initial thoughts on twilio

--- toboggan/app/controllers/notifications_controller.rb
+++ toboggan/app/controllers/notifications_controller.rb
@@ -1,4 +1,16 @@
 class NotificationsController < ApplicationController
 
-end
 
+  @twilio_number = "+17734926465"
+
+  def sms    #post route
+    content_type 'text/xml'
+
+    puts params
+    "<Response><Message>Hello</Message></Response>"
+  end
+
+
+
+
+end
