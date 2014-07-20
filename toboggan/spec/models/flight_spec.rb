d2d177a200ee784231cb652240d9a06132b8f58c
adriansoghoian <adriansoghoian@gmail.com>
2014-07-20 19:29:38 -0400

    airport-code-lookup

--- toboggan/spec/models/flight_spec.rb
+++ toboggan/spec/models/flight_spec.rb
@@ -5,8 +5,9 @@ describe Flight do
   it { should belong_to(:user) }
   it { should validate_presence_of(:beginning_airport) }
   it { should validate_presence_of(:ending_airport) }
-  it { should validate_numericality_of(:price) }
+  it { should validate_presence_of(:price) }
 
+  it { should allow_value('917-957-7347', '917-734-2345').for(:phone_number) }
   # it "should calculate duration on save" do
   #   f = FactoryGirl.build :flight
   #   f.start_vacation = Time.new(2014,12,30)
