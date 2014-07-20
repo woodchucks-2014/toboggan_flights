0676ebbc680ecbc61f882646f16a81022f25089f
Matthieu Gavaudan <mgavaudan@hotmail.com>
2014-07-20 18:26:30 -0400

    Got Twilio working :D

--- toboggan/app/controllers/notifications_controller.rb
+++ toboggan/app/controllers/notifications_controller.rb
@@ -1,15 +1,16 @@
 class NotificationsController < ApplicationController
 
-require 'twilio-ruby'
+  require 'twilio-ruby'
+  require 'dotenv'
+  Dotenv.load
 
-class TwilioController < ApplicationController
-  include Webhookable
 
   after_filter :set_header
 
   skip_before_action :verify_authenticity_token
 
   def voice
+
     response = Twilio::TwiML::Response.new do |r|
       r.Say 'Hey there. Congrats on integrating Twilio into your Rails 4 app.', :voice => 'alice'
          r.Play 'http://linode.rabasa.com/cantina.mp3'
@@ -17,7 +18,19 @@ class TwilioController < ApplicationController
 
     render_twiml response
   end
-end
 
+
+  def send_sms(body, phone_number)   #post route
+
+    twilio_token = ENV["TWILIO_API_KEY"]
+    twilio_sid = ENV["TWILIO_ACC_SID"]
+    @client = Twilio::REST::Client.new twilio_sid, twilio_token
+
+    @client.account.messages.create(
+        :from => '+18454434529',
+        :to => phone_number,
+        :body => body,
+      )
+  end
 
 end
