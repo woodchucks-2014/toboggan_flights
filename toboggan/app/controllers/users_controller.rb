f823d26a179f4677aac3c3c00430f440b3de93a9
QuigProQuo <johnpeterquigley@gmail.com>
2014-07-21 00:16:15 -0400

    Merge from master

--- toboggan/app/controllers/users_controller.rb
+++ toboggan/app/controllers/users_controller.rb
@@ -5,16 +5,16 @@ class UsersController < ApplicationController
 	# member_signed_in?
 	# current_member
 	# member_session
-	def new
-  	@user = User.new
-	end
-
-	def create
-	  @user = User.new(params[:user])
-	  if @user.save
-	    redirect_to root_url, :notice => "Signed up!"
-	  else
-	    render "new"
-	  end
-	end
+	# def new
+  # 	@user = User.new
+	# end
+	#
+	# def create
+	#   @user = User.new(params[:user])
+	#   if @user.save
+	#     redirect_to root_url, :notice => "Signed up!"
+	#   else
+	#     render "new"
+	#   end
+	# end
 end
