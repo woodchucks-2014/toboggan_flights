10a232e8337451718dfcb2bcdcd39d69d3911edb
Natalie Baer <ncbaer@gmail.com>
2014-07-20 22:06:14 -0400

    edits to user

--- toboggan/app/controllers/users_controller.rb
+++ toboggan/app/controllers/users_controller.rb
@@ -1,4 +1,28 @@
 class UsersController < ApplicationController
 	before_action :authenticate_user!
 
+	def show
+		@flights = Flight.where(current_user)
+
+	end
+
+	def new
+
+	end
+
+	def create
+
+	end
+
+	def update
+
+	end
+
+	# sign_out @user 
+
+	def destroy
+		@user.destroy
+		redirect_to "/"
+	end
+
 end
--- toboggan/config/routes.rb
+++ toboggan/config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
+  # devise_for :users do get '/users/sign_out' => 'devise/sessions#destroy' end
+
+  resources :flights
+  
+  devise_scope :user do
+    get 'departments/:department_id/sign_in' => 'devise/sessions#new', :as => :department_user_sign_in
+  end
 
   root 'flights#new'
   post "/flights", to: "flights#create"
   
-  resources :flights
-  resources :trips
+  # resources :users do
+  #   resources :flights
+  # end
+  resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
